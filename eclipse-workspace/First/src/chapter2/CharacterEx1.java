8cc223dcf3ac433e63b9d4acf448ce498003cdcb
teller2016 <teller2016@ajou.ac.kr>
2021-11-13 17:18:02 +0900

    chapter 2 on

---
+++ eclipse-workspace/First/src/chapter2/CharacterEx1.java
@@ -0,0 +1,18 @@
+package chapter2;
+
+public class CharacterEx1 {
+	public static void main(String[] args) {
+		char ch1 = 'A';
+		System.out.println(ch1);		//문자 출력
+		System.out.println((int)ch1);	//문자에 해당하는 아스키코드값 출력
+		
+		char ch2 = 66;					
+		System.out.println(ch2);		//정수값에 해당하는 문자 출력
+		
+		int ch3 = 67;
+		System.out.println(ch3);
+		System.out.println((char)ch3);	//정수값에 해당하는 문자 출력
+		
+		
+	}
+}
---
+++ eclipse-workspace/First/src/chapter2/Variable1.java
@@ -0,0 +1,13 @@
+package chapter2;
+
+public class Variable1 {
+
+	public static void main(String[] args) {
+		int level;	//정수형 변수 level 선언
+		level = 10;	//level 변수에 10 대입
+		
+		int age = 25;
+		System.out.println(level);
+	}
+
+}
