0ceb21d4e3f09c88cb664ab9514bfec32118eadc
teller2016 <teller2016@ajou.ac.kr>
2021-11-19 00:21:25 +0900

    chapter5 on

---
+++ eclipse-workspace/Chapter5/src/classpart/FunctionTest.java
@@ -0,0 +1,17 @@
+package classpart;
+
+public class FunctionTest {
+	public static void main(String[] arg) {
+		int num1 = 10;
+		int num2 = 20;
+		
+		int sum = add(num1, num2);
+		System.out.println(sum);
+	}
+	
+	public static int add(int n1, int n2) {
+		int result = n1 + n2;
+		return result;
+	}
+}
+
--- eclipse-workspace/Chapter5/src/classpart/Student.java
+++ eclipse-workspace/Chapter5/src/classpart/Student.java
@@ -6,6 +6,14 @@ public class Student {	//클래스 이름
 	int grade;
 	String address;
 	
+	public String getStudentName() {
+		return studentName;
+	}
+	
+	public void setStudentName(String name) {
+		studentName = name;
+	}
+	
 	public void showStudentInfo() {
 		System.out.println(studentName + ", " + address);
 	}
