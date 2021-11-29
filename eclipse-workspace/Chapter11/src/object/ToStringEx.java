99e34953365512b3a5463160400cde6f6eb339ec
teller2016 <teller2016@ajou.ac.kr>
2021-11-29 21:32:13 +0900

    chapter11 on

---
+++ eclipse-workspace/Chapter11/src/object/ToStringEx.java
@@ -0,0 +1,24 @@
+package object;
+class Book{
+	int bookNumber;
+	String bookTitle;
+	
+	Book(int bookNumber, String bookTitle){
+		this.bookNumber = bookNumber;
+		this.bookTitle = bookTitle;
+	}
+
+	@Override
+	public String toString() {
+		return bookTitle + "," + bookNumber;
+	}
+}
+public class ToStringEx {
+	public static void main(String[] arg) {
+		Book book1 = new Book(200, "°³¹Ì");
+		
+		System.out.println(book1);
+		System.out.println(book1.toString());
+	}
+
+}
